d3c646fd3240868faa4b7ce5d1329f29449ff9aa
wazaniq <vechesumstudent@gmail.com>
2016-11-29 18:22:17 +0200

    Saving only vehicle ObjectId to DB(working on it)
    Create-advert wasn't working properly(fixed)
    Vehicle model changed(for good i hope)
    Advert-data changed(now it is getting the vehicle object)
    Vehicle-data changed(findByLocation method moved to advert-data)

---
+++ AutoTrade-NodeJS/data/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+
+module.exports = function(config, models) {
+    mongoose.Promise = global.Promise;
+
+    mongoose.connect(config.connectionString, (err) => {
+        if (err) {
+            console.log(err.message);
+        }
+    });
+
+    const db = mongoose.connection;
+
+    db.on('error', (err) => {
+        console.log('Connection failed\n!' + err);
+    })
+
+    db.on('open', () => {
+        console.log('Connection succsess!');
+    })
+
+    const messageData = require('./messages-data')(models.Message),
+        userData = require('./users-data')(models.User),
+        advertData = require('./adverts-data')(models.Advert),
+        vehicleData = require('./vehicles-data')(models.Vehicle);
+    return {
+        messageData,
+        userData,
+        advertData,
+        vehicleData
+    };
+};
