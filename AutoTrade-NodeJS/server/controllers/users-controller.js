aa61d8a436de8b20fc8a05ccd7936adf3e52d263
YavorYanchev <yavor_yanchev_bs@abv.bg>
2016-12-01 15:30:22 +0200

    Fixed user-controller

--- AutoTrade-NodeJS/server/controllers/users-controller.js
+++ AutoTrade-NodeJS/server/controllers/users-controller.js
@@ -2,20 +2,20 @@
 
  module.exports = {
      getLoginForm(req, res) {
-         res.status(200).send(res.render("login"));
+         res.status(200).render("login");
      },
      getProfile(req, res) {
          if (!req.isAuthenticated()) {
-             res.status(401).redirect(res.render("unauthorized"));
+             res.status(401).render("unauthorized");
          } else {
              const user = req.user;
-             res.status(200).send(res.render("profile"));
+             res.status(200).render("profile");
          }
      },
      getUnauthorized(req, res) {
          res.send(res.render("unauthorized"));
      },
      getRegisterForm(req, res) {
-         res.status(200).send(res.render("register"));
+         res.status(200).render("register");
      }
  }
