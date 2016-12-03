bef0ba78274d0d187052dccdfacbf4444393dad2
de3ka <derketo@abv.bg>
2016-12-03 10:44:09 +0200

    added search controller, working on it

---
+++ AutoTrade-NodeJS/server/controllers/search-controller.js
@@ -0,0 +1,65 @@
+'use strict'
+
+module.exports = data => {
+    function getBasicSearch(req, res) {
+
+        res.render("search/basic-search")
+    }
+
+    function getAdvancedSearch(req, res) {
+        res.render("search/advanced-search");
+    }
+
+    function getVehiclesBasicSearch(req) {
+        let year = req.body.year,
+            manufacturer = req.body.manufacturer,
+            price = req.body.price;
+
+        let query = {
+            'year': year,
+            'manufacturer': manufacturer,
+            'price': price
+        };
+        data.vehicleData.getFilteredVehicles(query)
+            .then(vehicles => {
+                return vehicles;
+            })
+    }
+
+    function basicSearchResults(req, res, next) {
+        let year = req.body.year,
+            location = req.body.location,
+            manufacturer = req.body.manufacturer,
+            price = req.body.price;
+
+        let query = {
+            'year': year,
+            'location': location,
+            'manufacturer': manufacturer,
+            'price': price
+        };
+        //let foundAdvertsByLocationIds = [];
+        //let foundVehicleIdsByLocations = [];
+        //let filteredVehiclesId = [];
+        let foundVehiclesIdsBySearch = [];
+
+        let vehicles = data.vehicleData.getFilteredVehicles(query)
+            .then(vehicles => {
+                // console.log(vehicles);
+                Object.keys(vehicles).forEach(function(key) {
+                    let currentVehicleId = vehicles[key]["_id"];
+                    foundVehiclesIdsBySearch.push(currentVehicleId);
+                });
+                console.log(foundVehiclesIdsBySearch);
+                return data.advertData.getAdvertByVehicleIds(foundVehiclesIdsBySearch);
+            }).then(adverts => {
+                data.advertData.findByLocation(adverts);
+                console.log(adverts);
+            })
+    }
+    return {
+        getBasicSearch,
+        getAdvancedSearch,
+        basicSearchResults
+    }
+}
