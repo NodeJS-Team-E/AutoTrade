b84a97401b72ebdb39fc72f8f972fa5deca7e8ff
de3ka <derketo@abv.bg>
2016-12-04 02:45:47 +0200

    search controller updated and working

--- AutoTrade-NodeJS/server/controllers/search-controller.js
+++ AutoTrade-NodeJS/server/controllers/search-controller.js
@@ -11,34 +11,39 @@ module.exports = data => {
 
     function basicSearchResults(req, res, next) {
         let year = req.body.year,
-            location = req.body.location,
             manufacturer = req.body.manufacturer,
-            price = req.body.price;
+            price = req.body.price,
+            category = req.body.category,
+            fuelType = req.body.fuelType,
+            transmission = req.body.transmission,
+            color = req.body.color,
+            mileage=req.body.mileage;
 
         let query = {
             'year': year,
-            'location': location,
             'manufacturer': manufacturer,
-            'price': price
+            'price': price,
+            'category': category,
+            'fuelType': fuelType,
+            'transmission': transmission,
+            'color':color,
+            'mileage':mileage
         };
         let foundVehiclesIdsBySearch = [];
         let foundAdvertsByLocation = [];
 
-        let vehicles = data.vehicleData.getFilteredVehicles(query)
+        data.vehicleData.getFilteredVehicles(query)
             .then(vehicles => {
-                // console.log(vehicles);
                 Object.keys(vehicles).forEach(function(key) {
                     let currentVehicleId = vehicles[key]["_id"];
                     foundVehiclesIdsBySearch.push(currentVehicleId);
                 });
-                // console.log(foundVehiclesIdsBySearch);
                 return data.advertData.getAdvertByVehicleIds(foundVehiclesIdsBySearch);
             }).then(adverts => {
-                Object.keys(adverts).forEach(function(key) {
-                    let currentAdvertLocation = adverts[key]["location"];
-                    if (currentAdvertLocation === location) {
-                        //    console.log(adverts);
-                    }
+                console.log(adverts);
+                res.render("search/search-results", {
+                    adverts: adverts,
+                    user: req.user
                 });
             }).catch((err) => console.log(err));
     }
