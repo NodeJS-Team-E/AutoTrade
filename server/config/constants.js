b674bc40d85e12b730ed5fb96d052ad8449806a5
YavorYanchev <yavor_yanchev_bs@abv.bg>
2016-12-05 17:58:56 +0200

    Changed port for deploy

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,9 +1,12 @@
 const connectionString = "mongodb://nodejsteame:teamenode2016@ds042459.mlab.com:42459/autotrade",
-    port = 3001,
+    //port = 3001,
+    port = process.env.PORT,
+    // console.log(port);
     path = require("path"),
     rootPath = path.normalize(__dirname + "/../../");
 
 module.exports = {
+    environment: process.env.NODE_ENV,
     connectionString: connectionString,
     port: port,
     rootPath: rootPath
